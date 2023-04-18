f01592cd060b5dcf48957777a97fb9cd4fad7361
Replit user <>
2023-04-18 05:15:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -106,8 +106,8 @@ void removeNode(TreeMap * tree, TreeNode* node) {
   }
   
   if(node->left == NULL && node->right == NULL){
-      if(tree->lower_than(node->pair->key,node->parent->pair->key) == 0) node->parent->right = NULL;
-      else node->parent->left = NULL;
+      if(tree->lower_than(node->pair->key,node->parent->pair->key) == 0) node->parent->left = NULL;
+      else node->parent->right = NULL;
       free(node);
       return;
   }
